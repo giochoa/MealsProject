29cec3fabd2007034b53b9e8d95960304cc13ba8
giochoa <giochoa0422@gmail.com>
2022-05-14 17:16:52 +0900

    spacer util  add

--- src/features/restaurants/components/restaurants-info-card.components.js
+++ src/features/restaurants/components/restaurants-info-card.components.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
+import { Spacer } from "../../../components/spacer/spacer";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -37,22 +38,19 @@ const Section = styled.View`
 const SectionEnd = styled.View`
   flex: 1;
   flex-direction: row;
-  justify-contents: flex-end;
-`;
-const Open = styled(SvgXml)`
-  flex-direction: row;
+  justify-content: flex-end;
 `;
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Jacks ramen",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.gransta.jp/gr_cswp/wp-content/uploads/2022/01/noodle_main.jpg",
     ],
     address = "sumida ku kamezawa",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
   const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
@@ -66,10 +64,18 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
               <SvgXml xml={star} width={20} height={20} />
             ))}
           </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                Closed Temporarily
+              </Text>
+            )}
+            <Spacer variant="left.large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer variant="left.large" />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+          </SectionEnd>
         </Section>
-        <SectionEnd>
-          {isOpenNow && <Open xml={open} width={20} height={20} />}
-        </SectionEnd>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
