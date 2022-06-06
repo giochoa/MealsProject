197189e7fa2614ad248b4e9c3f12979cc47ecb02
giochoa <giochoa0422@gmail.com>
2022-06-06 17:48:04 +0900

    adding mock files and icons

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import { theme } from "./src/infrastructure/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import { Ionicons } from "@expo/vector-icons";
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -20,6 +22,12 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
@@ -30,6 +38,16 @@ const Maps = () => (
     <Text>Maps</Text>
   </SafeArea>
 );
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+};
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -45,10 +63,10 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="RestaurantScreen" component={RestaurantsScreen} />
-            <Tab.Screen name="Settings" component={Settings} />
+          <Tab.Navigator screenOptions={createScreenOptions}>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Maps} />
+            <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
         </NavigationContainer>
         <ExpoStatusBar style="auto" />
