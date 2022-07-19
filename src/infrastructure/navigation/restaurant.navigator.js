4276b9606d2d4e81d95d6c9668ef838a77d43eca
giochoa <giochoa0422@gmail.com>
2022-07-19 17:43:04 +0900

    Scroll and List added for the menu

--- src/infrastructure/navigation/restaurant.navigator.js
+++ src/infrastructure/navigation/restaurant.navigator.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "react-native-gesture-handler";
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { RestaurantDetailsScreen } from "../../features/restaurants/screens/resataurant-details-screen";
 
 const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator headerMode="off">
+    <RestaurantStack.Navigator
+      headerMode="off"
+      screenOptions={{
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
+    >
       <RestaurantStack.Screen name="Restaurant" component={RestaurantsScreen} />
+      <RestaurantStack.Screen
+        name="RestaurantDetails"
+        component={RestaurantDetailsScreen}
+      />
     </RestaurantStack.Navigator>
   );
 };
