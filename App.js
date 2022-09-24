a53ab9ba3846061fdbc6201c74e71deef4d5e748
giochoa <giochoa0422@gmail.com>
2022-09-24 16:21:00 +0900

    firebase integration test

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import { Text } from "react-native";
 
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
@@ -10,6 +10,10 @@ import { Navigation } from "./src/infrastructure/navigation/index";
 
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+// import * as firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 import {
   useFonts as useOswald,
@@ -17,8 +21,31 @@ import {
 } from "@expo-google-fonts/oswald";
 
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA3JS4nR-dsH6iZTYGx-HO8I25AknUPhLA",
+  authDomain: "mealstogo-fb4e7.firebaseapp.com",
+  projectId: "mealstogo-fb4e7",
+  storageBucket: "mealstogo-fb4e7.appspot.com",
+  messagingSenderId: "39538723071",
+  appId: "1:39538723071:web:9c582b0103edaeaf51a4ca",
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("@gmail.com", "")
+      .then((user) => {
+        console.log(user);
+        setIsAuthenticated(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
