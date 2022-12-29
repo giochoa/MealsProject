cc658354566d257d3c198c205b9225b8cd81e443
giochoa <giochoa0422@gmail.com>
2022-12-29 18:47:38 +0900

    change the watermelon

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -21,7 +21,7 @@ export const AccountScreen = ({ navigation }) => {
           autoPlay
           loop
           resizeMode="cover"
-          source={require("../../../../assets/watermelon.json")}
+          source={require("../../../../assets/boardman.json")}
         />
       </AnimationWrapper>
       <Title>Meals To Go</Title>
