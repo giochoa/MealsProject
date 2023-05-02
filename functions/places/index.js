ce68faa37ac9dc23967b3cc023944907ba80c632
giochoa <giochoa0422@gmail.com>
2023-05-02 16:23:29 +0900

    added the api for places and pics

--- functions/places/index.js
+++ functions/places/index.js
@@ -1,10 +1,46 @@
 const { mocks, addMockImages } = require("./mock");
-
-module.exports.placesRequest = (request, response) => {
-  const { location } = request.query;
-  const data = mocks[location];
-  if (data) {
-    data.results = data.results.map(addMockImages);
+const functions = require("firebase-functions");
+const addGoogleImages = (restaurant) => {
+  const ref = restaurant.photos[0].photo_reference;
+  if (!ref) {
+    restaurant.photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+    ];
+    return restaurant;
   }
-  response.json(data);
+  restaurant.photos = [
+    `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${
+      functions.config().google.key
+    }`,
+  ];
+  return restaurant;
+};
+
+module.exports.placesRequest = (request, response, client) => {
+  const { location, mock } = request.query;
+  if (mock === "true") {
+    const data = mocks[location];
+    if (data) {
+      data.results = data.results.map(addMockImages);
+    }
+    response.json(data);
+  }
+  client
+    .placesNearby({
+      params: {
+        location: location,
+        radius: 15000,
+        type: "restaurant",
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      res.data.results = res.data.results.map(addGoogleImages);
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
